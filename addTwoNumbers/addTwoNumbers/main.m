b17e53f05825ccb0a8e096aad66bedc4f93d0211
Michael Goodrum <michael@vlipsy.com>
2019-11-12 12:37:49 -0500

    finish exercise from chapter 1

---
+++ addTwoNumbers/addTwoNumbers/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  addTwoNumbers
+//
+//  Created by Michael Goodrum on 11/12/19.
+//  Copyright © 2019 Michael Goodrum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int x, y;
+        x = 10;
+        y = 12;
+        
+        NSLog(@"The sum of x and y is: %i", x + y);
+    }
+    return 0;
+}
